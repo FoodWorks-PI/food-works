0265e379c9cdc7b9b3cccf415f453b9cf13907f0
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2020-10-08 18:31:48 -0500

    Dummy

--- src/services/apollo.js
+++ src/services/apollo.js
@@ -6,12 +6,3 @@ export const apiClient = new ApolloClient({
   uri: 'https://127.0.0.1:4455/api/graphql',
   cache: new InMemoryCache(),
 });
-
-export const GET_CURRENT_USER = gql`
-  query {
-    getCurrentCustomer {
-      Name
-      Email
-    }
-  }
-`;
