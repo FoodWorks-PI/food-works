2621ed9e8351128d7a7e314ec20d586118920df3
Julian H <cjulianh46@gmail.com>
2020-10-08 19:44:48 -0600

    Mutations in front

--- src/App.react.js
+++ src/App.react.js
@@ -5,21 +5,108 @@ import logo from 'logo.svg';
 import 'App.css';
 import {useState} from 'react';
 import useNetworkState from 'hooks/useNetworkState';
-import {ApolloProvider} from '@apollo/client';
-import {apiClient} from 'services/apollo';
+import {ApolloProvider, useMutation} from '@apollo/client';
+import {apiClient, GET_CURRENT_USER, CREATE_USER} from 'services/apollo';
 import {kratos} from 'services/kratos';
 import {
   BrowserRouter as Router,
+  Link,
   Route, // for later
   Switch,
 } from 'react-router-dom';
 
+const client = apiClient;
+
+function RequestDemo() {
+  const [createUser] = useMutation(CREATE_USER);
+  function createUserClick() {
+    createUser({
+      variables: {
+        input: {
+          name: 'Alfredo',
+          phone: '1111111',
+          address: {
+            latitude: 1.0,
+            longitude: 2.0,
+            streetLine: 'Casa',
+          },
+        },
+      },
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={getSession}>Get Session</button>
+      <button onClick={getUser}>Get User</button>
+      <button onClick={createUserClick}>New User</button>
+    </div>
+  );
+}
+
+function getSession() {
+  kratos.whoami().then((result) => console.log(result));
+}
+
+function getUser() {
+  client
+    .query({
+      query: GET_CURRENT_USER,
+    })
+    .then((result) => console.log(result));
+}
+
 function App() {
+  const [isOnline, connectedAt] = useNetworkState();
+  const [count, setCount] = useState(0);
+
+  function updateCount(): void {
+    setCount(count + 1);
+  }
+
+  function PublicDemo() {
+    return (
+      <div>
+        <a href="https://127.0.0.1:4455/auth/registration?clientApp=https://127.0.1:4455:/customer/protected">
+          Register
+        </a>
+        <a href="https://127.0.0.1:4455/auth/login?clientApp=https://127.0.0.1:4455/customer/protected">
+          Login
+        </a>
+        <RequestDemo />
+      </div>
+    );
+  }
+
+  function ProtectedDemo() {
+    return (
+      <div>
+        <RequestDemo />
+      </div>
+    );
+  }
+
+  function HomeDemo() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{isOnline ? 'Connected' : 'Disconnected'}</p>
+        {isOnline ? connectedAt?.toDateString() : null}
+        <button onClick={updateCount}>{count}</button>
+      </header>
+    );
+  }
+
   return (
     <ApolloProvider client={apiClient}>
       <div className="App">
         <Router>
-          <Switch></Switch>
+          <Switch>
+            <Route path="/home" component={HomeDemo} />
+            <Route path="/public" component={PublicDemo} />
+            <Route path="/customer/public" component={PublicDemo} />
+            <Route path="/customer/protected" component={ProtectedDemo} />
+          </Switch>
         </Router>
       </div>
     </ApolloProvider>
--- src/services/apollo.js
+++ src/services/apollo.js
@@ -6,3 +6,19 @@ export const apiClient = new ApolloClient({
   uri: 'https://127.0.0.1:4455/api/graphql',
   cache: new InMemoryCache(),
 });
+
+export const CREATE_USER = gql`
+  mutation CreateUser($input: RegisterCustomerInput!) {
+    createCustomerProfile(input: $input)
+  }
+`;
+
+export const GET_CURRENT_USER = gql`
+  query {
+    getCurrentCustomer {
+      name
+      email
+      phone
+    }
+  }
+`;
