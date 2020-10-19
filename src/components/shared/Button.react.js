788a69815a2ffade6d435d36649ed45eda14ed59
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2020-10-18 23:08:43 -0500

    Added some fun styles to the map

--- src/components/shared/Button.react.js
+++ src/components/shared/Button.react.js
@@ -4,11 +4,12 @@ import React from 'react';
 import type {Node} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import clsx from 'clsx';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles({
-  root: {
+  root: (props) => ({
     background: 'none',
-    backgroundColor: '#7ea8ef',
+    backgroundColor: props.isDisabled ? '#99b9ef' : '#7ea8ef',
     color: '#fff',
     lineHeight: 1.42858,
     border: 'none',
@@ -25,27 +26,41 @@ const useStyles = makeStyles({
     justifyContent: 'center',
     borderColor: 'rgb(0,0,0,0.4)',
     '&:active': {
-      backgroundColor: '#6b9cef',
+      backgroundColor: !props.isDisabled ? '#6b9cef' : '#99b9ef',
     },
-  },
+  }),
+  content: (props) => ({
+    opacity: props.isLoading ? 0 : 1,
+  }),
 });
 
 type Props = $ReadOnly<{
-  onClick: (e: SyntheticMouseEvent<>) => mixed,
+  onClick?: ?(e: SyntheticMouseEvent<>) => mixed,
   children: Node,
   className?: ?string,
+  isLabelHidden?: boolean,
+  isDisabled?: boolean,
+  isLoading?: boolean,
 }>;
 
-function Button(props: Props): Node {
-  const classes = useStyles();
+function Button({
+  onClick,
+  children,
+  className,
+  isDisabled = false,
+  isLabelHidden = false,
+  isLoading = false,
+}: Props): Node {
+  const classes = useStyles({isDisabled, isLoading});
 
   return (
     <div
-      className={clsx(classes.root, props.className)}
-      onClick={props.onClick}
+      className={clsx(classes.root, className)}
+      onClick={!isDisabled ? onClick : undefined}
       role="button"
     >
-      <span>{props.children}</span>
+      {isLabelHidden ? '\u200B' : <span className={classes.content}>{children}</span>}
+      {isLoading && <CircularProgress size={16} />}
     </div>
   );
 }
--- src/components/user_creation/UserLocationStep.react.js
+++ src/components/user_creation/UserLocationStep.react.js
@@ -13,6 +13,10 @@ import {useDispatch, useMappedState} from 'stores/hooks/CustomerStoreHooks';
 import {loadPlaceFromReverse} from 'actions/CustomerSelectedPlaceActions';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import SearchIcon from '@material-ui/icons/Search';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Button from 'components/shared/Button.react';
 
 const barHeight = 104;
 
@@ -38,6 +42,41 @@ const useStyles = makeStyles({
     height: `calc(100vh - ${barHeight}px)`,
     position: 'relative',
   },
+  buttonToAutocomplete: {
+    position: 'absolute',
+    zIndex: 1,
+    top: 24,
+    width: '80%',
+    left: '50%',
+    transform: 'translate(-50%, 0%)',
+    borderRadius: 20,
+  },
+  searchCard: {
+    borderRadius: 20,
+    width: '100%',
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 8,
+    paddingTop: 8,
+  },
+  searchIcon: {
+    marginRight: 8,
+  },
+  locationIcon: {
+    position: 'absolute',
+    zIndex: 1,
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  buttonReady: {
+    position: 'absolute',
+    zIndex: 1,
+    bottom: 48,
+    width: '80%',
+    left: '50%',
+    transform: 'translate(-50%, 0%)',
+  },
 });
 
 export default function UserLocationStep(): Node {
@@ -46,6 +85,7 @@ export default function UserLocationStep(): Node {
   const classes = useStyles();
   const [map, setMap] = useState(null);
   const [userPosition] = useInitialGeoPosition();
+  const [loadingCreation, setLoadingCreation] = useState(false);
 
   useEffect(() => {
     if (userPosition) {
@@ -70,6 +110,10 @@ export default function UserLocationStep(): Node {
       );
     }
   }, [map, dispatch]);
+
+  function onReadyClick() {
+    setLoadingCreation(true);
+  }
 
   return (
     <FlexLayout className={classes.root} direction="vertical">
@@ -101,6 +145,28 @@ export default function UserLocationStep(): Node {
           onLoad={onLoad}
           onIdle={onIdle}
         />
+        <LocationOnIcon
+          className={classes.locationIcon}
+          color="primary"
+          fontSize="large"
+        />
+        <ButtonBase className={classes.buttonToAutocomplete}>
+          <Paper className={classes.searchCard} elevation={4}>
+            <FlexLayout align="center">
+              <SearchIcon className={classes.searchIcon} />
+              <Typography variant="body2">Busca un lugar...</Typography>
+            </FlexLayout>
+          </Paper>
+        </ButtonBase>
+        <Button
+          className={classes.buttonReady}
+          isDisabled={loadingCreation}
+          isLoading={loadingCreation}
+          isLabelHidden={loadingCreation}
+          onClick={onReadyClick}
+        >
+          Continuar
+        </Button>
       </div>
     </FlexLayout>
   );
