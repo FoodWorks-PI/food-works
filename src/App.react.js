f2c7b0b528e2438d3d0d8641a1f0c1caa5e2bac2
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2020-10-27 22:52:51 -0600

    added onboarding component

--- src/App.react.js
+++ src/App.react.js
@@ -1,8 +1,6 @@
 // @flow
 
-import type {Node} from 'react';
-
-import React from 'react';
+import * as React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {ThemeProvider, CssBaseline} from '@material-ui/core';
 import {LoadScript} from '@react-google-maps/api';
@@ -13,8 +11,10 @@ import UserExistsRenderer from 'components/shared/UserExistsRenderer.react';
 import UserNameStep from 'components/user_creation/UserNameStep.react';
 import {ApolloProvider} from '@apollo/client';
 import {client} from 'services/Apollo';
+import * as ROUTES from 'constants/Routes';
+import Onboarding from 'components/onboarding/Onboarding.react';
 
-function App(): Node {
+function App(): React.Node {
   return (
     <ApolloProvider client={client}>
       <LoadScript googleMapsApiKey={process.env.REACT_APP_MAPS_KEY}>
@@ -23,13 +23,16 @@ function App(): Node {
             <CssBaseline />
             <Router>
               <Switch>
-                <Route exact path="/customer/protected/">
+                <Route exact path={ROUTES.PUBLIC_ROOT}>
+                  <Onboarding />
+                </Route>
+                <Route exact path={ROUTES.PROTECTED_ROOT}>
                   {/* TODO: Write a more user friendly fallback */}
                   <UserExistsRenderer fallback={<p>Loading...</p>}>
                     <div>User Created!</div>
                   </UserExistsRenderer>
                 </Route>
-                <Route exact path="/customer/protected/create/one/">
+                <Route exact path={ROUTES.CREATE_ONE}>
                   <UserNameStep />
                 </Route>
               </Switch>
--- src/constants/Routes.js
+++ src/constants/Routes.js
@@ -1,2 +1,4 @@
 // @flow strict
-export const CREATE_ONE = '/customer/protected/create/one/';
+export const CREATE_ONE = '/customer/protected/create/one';
+export const PROTECTED_ROOT = '/customer/protected';
+export const PUBLIC_ROOT = '/customer';
