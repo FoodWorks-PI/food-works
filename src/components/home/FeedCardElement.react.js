8292c2f2898208778909917f03ff9b4e172c3c15
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2020-11-29 21:15:08 -0600

    added average rating

--- src/components/home/FeedCardElement.react.js
+++ src/components/home/FeedCardElement.react.js
@@ -4,6 +4,7 @@ import type {FeedCard} from 'constants/FeedTypes';
 
 import * as React from 'react';
 import ProductCard from 'components/shared/ProductCard.react';
+import RestaurantCard from 'components/shared/RestaurantCard.react';
 
 type Props = {
   item: FeedCard,
@@ -13,6 +14,10 @@ export default function FeedCardElement({item}: Props): React.Node {
   if (item.__typename === 'Product') {
     // $ExpectError
     return <ProductCard product={{...item}} />;
+  }
+
+  if (item.__typename === 'Restaurant') {
+    return <RestaurantCard restaurant={{...item}} />;
   }
   return <div />;
 }
